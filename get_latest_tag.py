6d1115bb6f676bb90f7911fa485beaf6ad8b5d05
Bernardo Heynemann <heynemann@gmail.com>
2016-06-08 10:56:24 -0300

    Get latest tag from docker hub

---
+++ get_latest_tag.py
@@ -0,0 +1,43 @@
+import urllib
+import urllib2
+import json
+
+def main():
+    url = "https://auth.docker.io/token?service=registry.docker.io&scope=repository:tfgco/khan:pull,push"
+    response = urllib.urlopen(url)
+    token = json.loads(response.read())['token']
+
+    url = "https://registry-1.docker.io/v2/tfgco/khan/tags/list"
+    req = urllib2.Request(url, None, {
+        "Authorization": "Bearer %s" % token,
+    })
+    response = urllib2.urlopen(req)
+    tags = json.loads(response.read())
+    last_tag = get_last_tag(tags['tags'])
+    print last_tag
+
+
+def get_tag_value(tag):
+    while len(tag) < 4:
+        tag.append('0')
+
+    total_value = 0
+    for index, tag_part in enumerate(tag):
+        power = pow(100, len(tag) - index)
+        total_value += int(tag_part) * power
+
+    return total_value
+
+
+def get_last_tag(tags):
+    return '.'.join(
+        max([
+            (get_tag_value(tag), tag) for tag in 
+                [t.split('.') for t in tags]
+            ], key=lambda i: i[0]
+        )[1]
+    )
+
+
+if __name__ == "__main__":
+    main()
