3df27def98276d0b10823a66a221f011a134f4ff
Bernardo Heynemann <heynemann@gmail.com>
2016-06-08 11:06:36 -0300

    Validate that latest version is indeed in repository

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -6,3 +6,11 @@ docker build -t khan .
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker tag khan:latest tfgco/khan:$VERSION.$TRAVIS_BUILD_NUMBER
 docker push tfgco/khan:$VERSION.$TRAVIS_BUILD_NUMBER
+
+DOCKERHUB_LATEST=$(python get_latest_tag.py)
+
+if [ "$DOCKERHUB_LATEST" != "$VERSION.$TRAVIS_BUILD_NUMBER" ]; then
+    echo "Last version is not in docker hub!"
+    echo "docker hub: $DOCKERHUB_LATEST, expected: $VERSION.$TRAVIS_BUILD_NUMBER"
+    exit 1
+fi
