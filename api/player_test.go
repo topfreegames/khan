c6f464c82291fc990e6aa87ecff09f91108a8aa1
Bernardo Heynemann <heynemann@gmail.com>
2016-06-09 17:56:52 -0300

    Player handler all tested

--- api/player_test.go
+++ api/player_test.go
@@ -61,6 +61,31 @@ func TestPlayerHandler(t *testing.T) {
 			var result map[string]interface{}
 			json.Unmarshal([]byte(res.Body().Raw()), &result)
 			g.Assert(result["success"]).IsFalse()
+			g.Assert(result["reason"]).Equal(
+				"\n[IRIS]  Error: While trying to read [JSON invalid character 'i' looking for beginning of value] from the request body. Trace %!!(MISSING)s(MISSING)",
+			)
+		})
+
+		g.It("Should not create player if invalid data", func() {
+			gameID := "game-id-is-too-large-for-this-field-should-be-less-than-10-chars"
+			playerID := randomdata.FullName(randomdata.RandomGender)
+			playerName := randomdata.FullName(randomdata.RandomGender)
+			metadata := "{\"x\": 1}"
+
+			a := GetDefaultTestApp()
+			payload := map[string]string{
+				"gameID":   gameID,
+				"playerID": playerID,
+				"name":     playerName,
+				"metadata": metadata,
+			}
+			res := PostJSON(a, "/players", t, payload)
+
+			res.Status(http.StatusInternalServerError)
+			var result map[string]interface{}
+			json.Unmarshal([]byte(res.Body().Raw()), &result)
+			g.Assert(result["success"]).IsFalse()
+			g.Assert(result["reason"]).Equal("pq: value too long for type character varying(10)")
 		})
 	})
 }
