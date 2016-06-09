48d6fd31990406850f8e2ef486a0a7926014617b
Bernardo Heynemann <heynemann@gmail.com>
2016-06-09 15:30:09 -0300

    licensing for errors

--- models/errors.go
+++ models/errors.go
@@ -1,3 +1,10 @@
+// khan
+// https://github.com/topfreegames/khan
+//
+// Licensed under the MIT license:
+// http://www.opensource.org/licenses/mit-license
+// Copyright © 2016 Top Free Games <backend@tfgco.com>
+
 package models
 
 import "fmt"
