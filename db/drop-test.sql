c8e1d4bdc9719161a288794607b08ed64fd5643a
cscatolini <camila.scatolini@gmail.com>
2016-06-09 14:36:04 -0300

    Fix readme and add copyright info to all files.

--- db/drop-test.sql
+++ db/drop-test.sql
@@ -1,14 +1,21 @@
+-- khan
+-- https://github.com/topfreegames/khan
+--
+-- Licensed under the MIT license:
+-- http://www.opensource.org/licenses/mit-license
+-- Copyright © 2016 Top Free Games <backend@tfgco.com>
+
 REVOKE ALL ON SCHEMA public FROM khan_test;
 DROP DATABASE IF EXISTS khan_test;
 DROP ROLE khan_test;
- 
+
 CREATE ROLE khan_test LOGIN
   SUPERUSER INHERIT CREATEDB CREATEROLE;
- 
+
 CREATE DATABASE khan_test
   WITH OWNER = khan_test
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
        TEMPLATE = template0;
- 
+
 GRANT ALL ON SCHEMA public TO khan_test;
--- db/drop.sql
+++ db/drop.sql
@@ -1,14 +1,21 @@
+-- khan
+-- https://github.com/topfreegames/khan
+--
+-- Licensed under the MIT license:
+-- http://www.opensource.org/licenses/mit-license
+-- Copyright © 2016 Top Free Games <backend@tfgco.com>
+
 REVOKE ALL ON SCHEMA public FROM khan;
 DROP DATABASE IF EXISTS khan;
 DROP ROLE khan;
- 
+
 CREATE ROLE khan LOGIN
   SUPERUSER INHERIT CREATEDB CREATEROLE;
- 
+
 CREATE DATABASE khan
   WITH OWNER = khan
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
        TEMPLATE = template0;
- 
+
 GRANT ALL ON SCHEMA public TO khan;
