4ab5bc45711e4d51e78b900fa2ba009b6a154cd1
Bernardo Heynemann <heynemann@gmail.com>
2016-06-09 14:17:37 -0300

    Healthcheck now verifies database connection

--- api/healthcheck.go
+++ api/healthcheck.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/kataras/iris"
 )
 
@@ -8,6 +10,13 @@ import (
 func HealthCheckHandler(app *App) func(c *iris.Context) {
 	return func(c *iris.Context) {
 		workingString := app.Config.GetString("healthcheck.workingText")
+		num, err := app.Db.SelectInt("select 1")
+		if num != 1 || err != nil {
+			c.Write(fmt.Sprintf("Error connecting to database: %s", err))
+			c.SetStatusCode(500)
+			return
+		}
+
 		c.SetStatusCode(iris.StatusOK)
 		c.Write(workingString)
 	}
