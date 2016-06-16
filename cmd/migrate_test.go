c179b4fbca78454de4eeb9ad5ebd6c7fbefc5420
Bernardo Heynemann <heynemann@gmail.com>
2016-06-16 16:22:14 -0300

    Migration command with somet tests

---
+++ cmd/migrate_test.go
@@ -0,0 +1,51 @@
+// khan
+// https://github.com/topfreegames/khan
+//
+// Licensed under the MIT license:
+// http://www.opensource.org/licenses/mit-license
+// Copyright © 2016 Top Free Games <backend@tfgco.com>
+
+package cmd
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+
+	. "github.com/franela/goblin"
+)
+
+func dropDB() error {
+	cmd := exec.Cmd{
+		Dir:  "../",
+		Path: "/usr/bin/make",
+		Args: []string{
+			"drop-test",
+		},
+	}
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TestMigrationCommand(t *testing.T) {
+	g := Goblin(t)
+
+	g.Describe("Migrate Cmd", func() {
+		g.BeforeEach(func() {
+			err := dropDB()
+			g.Assert(err == nil).IsTrue()
+		})
+
+		g.It("Should run migrations up", func() {
+			ConfigFile = "../config/test.yaml"
+			initConfig()
+			err := runMigrations("../db/migrations", -1)
+			fmt.Println(err)
+			g.Assert(err == nil).IsTrue()
+		})
+	})
+}
