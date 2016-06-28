f3a3378c5960dd4c5e255dfb503e8feae339fb84
cscatolini <camila.scatolini@gmail.com>
2016-06-28 09:05:13 -0300

    Membership perf tests.

---
+++ bench/membership_test.go
@@ -0,0 +1,275 @@
+// khan
+// https://github.com/topfreegames/khan
+//
+// Licensed under the MIT license:
+// http://www.opensource.org/licenses/mit-license
+// Copyright © 2016 Top Free Games <backend@tfgco.com>
+
+package bench
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/topfreegames/khan/models"
+	"github.com/topfreegames/khan/util"
+)
+
+var membershipResult *http.Response
+
+func BenchmarkApplyForMembership(b *testing.B) {
+	db, err := models.GetPerfDB()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	game, clan, _, _, _, err := models.GetClanWithMemberships(db, 20, 20, 20, 20, "", "")
+	if err != nil {
+		panic(err.Error())
+	}
+	game.MaxMembers = 20 + b.N
+	_, err = db.Update(game)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clan.AllowApplication = true
+	_, err = db.Update(clan)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var players []*models.Player
+	for i := 0; i < b.N; i++ {
+		player := models.PlayerFactory.MustCreateWithOption(util.JSON{
+			"GameID": game.PublicID,
+		}).(*models.Player)
+		err = db.Insert(player)
+		if err != nil {
+			panic(err.Error())
+		}
+		players = append(players, player)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		payload := util.JSON{
+			"level":          "Member",
+			"playerPublicID": players[i].PublicID,
+		}
+		route := getRoute(fmt.Sprintf("/games/%s/clans/%s/memberships/application", game.PublicID, clan.PublicID))
+		res, err := postTo(route, payload)
+		validateResp(res, err)
+		res.Body.Close()
+
+		membershipResult = res
+	}
+}
+
+func BenchmarkInviteForMembership(b *testing.B) {
+	db, err := models.GetPerfDB()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	game, clan, owner, _, _, err := models.GetClanWithMemberships(db, 20, 20, 20, 20, "", "")
+	if err != nil {
+		panic(err.Error())
+	}
+	game.MaxMembers = 20 + b.N
+	_, err = db.Update(game)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var players []*models.Player
+	for i := 0; i < b.N; i++ {
+		player := models.PlayerFactory.MustCreateWithOption(util.JSON{
+			"GameID": game.PublicID,
+		}).(*models.Player)
+		err = db.Insert(player)
+		if err != nil {
+			panic(err.Error())
+		}
+		players = append(players, player)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		payload := util.JSON{
+			"level":             "Member",
+			"playerPublicID":    players[i].PublicID,
+			"requestorPublicID": owner.PublicID,
+		}
+		route := getRoute(fmt.Sprintf("/games/%s/clans/%s/memberships/invitation", game.PublicID, clan.PublicID))
+		res, err := postTo(route, payload)
+		validateResp(res, err)
+		res.Body.Close()
+
+		membershipResult = res
+	}
+}
+
+func BenchmarkApproveMembershipApplication(b *testing.B) {
+	db, err := models.GetPerfDB()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	game, clan, owner, players, memberships, err := models.GetClanWithMemberships(db, 0, 0, 0, b.N, "", "")
+	if err != nil {
+		panic(err.Error())
+	}
+	game.MaxMembers = b.N
+	_, err = db.Update(game)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for i := 0; i < b.N; i++ {
+		memberships[i].RequestorID = memberships[i].PlayerID
+		_, err = db.Update(memberships[i])
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		payload := util.JSON{
+			"playerPublicID":    players[i].PublicID,
+			"requestorPublicID": owner.PublicID,
+		}
+		route := getRoute(fmt.Sprintf("/games/%s/clans/%s/memberships/application/approve", game.PublicID, clan.PublicID))
+		res, err := postTo(route, payload)
+		validateResp(res, err)
+		res.Body.Close()
+
+		membershipResult = res
+	}
+}
+
+func BenchmarkApproveMembershipInvitation(b *testing.B) {
+	db, err := models.GetPerfDB()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	game, clan, _, players, _, err := models.GetClanWithMemberships(db, 0, 0, 0, b.N, "", "")
+	if err != nil {
+		panic(err.Error())
+	}
+	game.MaxMembers = b.N
+	_, err = db.Update(game)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		payload := util.JSON{
+			"playerPublicID":    players[i].PublicID,
+			"requestorPublicID": players[i].PublicID,
+		}
+		route := getRoute(fmt.Sprintf("/games/%s/clans/%s/memberships/invitation/approve", game.PublicID, clan.PublicID))
+		res, err := postTo(route, payload)
+		validateResp(res, err)
+		res.Body.Close()
+
+		membershipResult = res
+	}
+}
+
+func BenchmarkDeleteMembership(b *testing.B) {
+	db, err := models.GetPerfDB()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	game, clan, owner, players, _, err := models.GetClanWithMemberships(db, b.N, 0, 0, 0, "", "")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		payload := util.JSON{
+			"playerPublicID":    players[i].PublicID,
+			"requestorPublicID": owner.PublicID,
+		}
+		route := getRoute(fmt.Sprintf("/games/%s/clans/%s/memberships/delete", game.PublicID, clan.PublicID))
+		res, err := postTo(route, payload)
+		validateResp(res, err)
+		res.Body.Close()
+
+		membershipResult = res
+	}
+}
+
+func BenchmarkPromoteMembership(b *testing.B) {
+	db, err := models.GetPerfDB()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	game, clan, owner, players, _, err := models.GetClanWithMemberships(db, b.N, 0, 0, 0, "", "")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		payload := util.JSON{
+			"playerPublicID":    players[i].PublicID,
+			"requestorPublicID": owner.PublicID,
+		}
+		route := getRoute(fmt.Sprintf("/games/%s/clans/%s/memberships/promote", game.PublicID, clan.PublicID))
+		res, err := postTo(route, payload)
+		validateResp(res, err)
+		res.Body.Close()
+
+		membershipResult = res
+	}
+}
+
+func BenchmarkDemoteMembership(b *testing.B) {
+	db, err := models.GetPerfDB()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	game, clan, owner, players, memberships, err := models.GetClanWithMemberships(db, b.N, 0, 0, 0, "", "")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for i := 0; i < b.N; i++ {
+		memberships[i].Level = "Elder"
+		_, err = db.Update(memberships[i])
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		payload := util.JSON{
+			"playerPublicID":    players[i].PublicID,
+			"requestorPublicID": owner.PublicID,
+		}
+		route := getRoute(fmt.Sprintf("/games/%s/clans/%s/memberships/demote", game.PublicID, clan.PublicID))
+		res, err := postTo(route, payload)
+		validateResp(res, err)
+		res.Body.Close()
+
+		membershipResult = res
+	}
+}
