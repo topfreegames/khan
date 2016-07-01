dafe44a4d5d4a2e3b772db4fbd1a8ae17136b954
cscatolini <camila.scatolini@gmail.com>
2016-07-01 11:16:53 -0300

    Dev docker image.

--- push_to_docker.sh
+++ push_to_docker.sh
@@ -3,9 +3,13 @@
 VERSION=$(cat ./api/version.go | grep "var VERSION" | awk ' { print $4 } ' | sed s/\"//g)
 
 docker build -t khan .
+docker build -t khan-dev ./dev
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+
 docker tag khan:latest tfgco/khan:$VERSION.$TRAVIS_BUILD_NUMBER
 docker push tfgco/khan:$VERSION.$TRAVIS_BUILD_NUMBER
+docker tag khan-dev:latest tfgco/khan-dev:$VERSION.$TRAVIS_BUILD_NUMBER
+docker push tfgco/khan-dev:$VERSION.$TRAVIS_BUILD_NUMBER
 
 DOCKERHUB_LATEST=$(python get_latest_tag.py)
 
