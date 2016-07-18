8757ba04c4fae82da5acfd005328c2336cd4b92c
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2016-07-18 12:00:34 -0300

    basic auth must be optionalg

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-
 echo configuring htpasswd
-htpasswd -b -c /etc/nginx/.htpasswd $AUTH_USERNAME $AUTH_PASSWORD
+if [ $USE_BASICAUTH == "true" ]; then
+  htpasswd -b -c /etc/nginx/.htpasswd $BASICAUTH_USERNAME $BASICAUTH_PASSWORD
+  cp ./docker/nginx_default_basicauth /etc/nginx/sites-enabled/default
+else
+  cp ./docker/nginx_default /etc/nginx/sites-enabled/default
+fi
 
 echo configuring nginx
 sed -i "s/{SERVER_NAME}/$SERVER_NAME/g" /etc/nginx/sites-enabled/default
