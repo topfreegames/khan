2d8b2409274e0909ac012d6696556467dba0cc79
Bernardo Heynemann <heynemann@gmail.com>
2016-08-29 13:26:04 -0300

    Postgres port

--- api/helpers_test.go
+++ api/helpers_test.go
@@ -22,12 +22,12 @@ import (
 
 // GetTestDB returns a connection to the test database
 func GetTestDB() (models.DB, error) {
-	return models.GetDB("localhost", "khan_test", 5432, "disable", "khan_test", "")
+	return models.GetDB("localhost", "khan_test", 5433, "disable", "khan_test", "")
 }
 
 // GetFaultyTestDB returns an ill-configured test database
 func GetFaultyTestDB() models.DB {
-	faultyDb, _ := models.InitDb("localhost", "khan_tet", 5432, "disable", "khan_test", "")
+	faultyDb, _ := models.InitDb("localhost", "khan_tet", 5433, "disable", "khan_test", "")
 	return faultyDb
 }
 
