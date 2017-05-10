1861337ab25939454818eabcaaa657c5b85f012f
cscatolini <camila.scatolini@gmail.com>
2017-05-10 18:46:39 -0300

    Fix port in run_migrations_and_start script.

--- run_migrations_and_start.sh
+++ run_migrations_and_start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 /go/bin/khan migrate -c /go/src/github.com/topfreegames/khan/config/default.yaml
-/bin/bash -c 'if [ "$KHAN_RUN_WORKER" != "true" ]; then /go/bin/khan start --bind 0.0.0.0 --port 80 --fast --config /go/src/github.com/topfreegames/khan/config/default.yaml; else /go/bin/khan worker --config /go/src/github.com/topfreegames/khan/config/default.yaml; fi'
+/bin/bash -c 'if [ "$KHAN_RUN_WORKER" != "true" ]; then /go/bin/khan start --bind 0.0.0.0 --port 8080 --config /go/src/github.com/topfreegames/khan/config/default.yaml; else /go/bin/khan worker --config /go/src/github.com/topfreegames/khan/config/default.yaml; fi'
