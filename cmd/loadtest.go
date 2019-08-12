e752d707e060dacd5f71986d84f22967e57aa98a
Matheus Pimenta <matheus.pimenta@tfgco.com>
2019-08-12 15:14:11 -0300

    CLI load test app should allow multiple goroutines for local tests with concurrency

--- cmd/loadtest.go
+++ cmd/loadtest.go
@@ -8,6 +8,7 @@ import (
 )
 
 var sharedClansFile string
+var nGoroutines int
 
 var loadtestCmd = &cobra.Command{
 	Use:   "loadtest",
@@ -17,19 +18,36 @@ You can use environment variables to override configuration keys.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		logger := zap.New(zap.NewJSONEncoder(), zap.InfoLevel)
 
+		exitChannel := make(chan bool)
+		routine := func() {
+			l := logger.With(
+				zap.String("source", "loadtestCmd"),
+				zap.String("operation", "Run/goroutine"),
+			)
+
+			app := loadtest.GetApp(ConfigFile, sharedClansFile, logger)
+			if err := app.Run(); err != nil {
+				log.E(l, "Goroutine exited with error.", func(cm log.CM) {
+					cm.Write(zap.String("error", err.Error()))
+				})
+			} else {
+				log.I(l, "Goroutine exited without errors.")
+			}
+
+			exitChannel <- true
+		}
+		for i := 0; i < nGoroutines; i++ {
+			go routine()
+		}
+		for i := 0; i < nGoroutines; i++ {
+			<-exitChannel
+		}
+
 		l := logger.With(
 			zap.String("source", "loadtestCmd"),
 			zap.String("operation", "Run"),
 		)
-
-		app := loadtest.GetApp(ConfigFile, sharedClansFile, logger)
-		if err := app.Run(); err != nil {
-			log.P(l, "Application exited with error.", func(cm log.CM) {
-				cm.Write(zap.String("error", err.Error()))
-			})
-		} else {
-			log.I(l, "Application exited without errors.")
-		}
+		log.I(l, "Application exited without errors.")
 	},
 }
 
@@ -41,5 +59,12 @@ func init() {
 		"clans",
 		"./config/loadTestSharedClans.yaml",
 		"shared clans list for load test",
+	)
+
+	loadtestCmd.Flags().IntVar(
+		&nGoroutines,
+		"goroutines",
+		1,
+		"number of goroutines to spawn for concurrent load tests",
 	)
 }
