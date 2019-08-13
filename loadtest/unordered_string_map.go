a06b16fcb7e58c4ca19f99eb330fe4b8c54da848
Matheus Pimenta <matheus.pimenta@tfgco.com>
2019-08-13 15:38:24 -0300

    Preventing a potential memory leak

--- loadtest/unordered_string_map.go
+++ loadtest/unordered_string_map.go
@@ -56,6 +56,7 @@ func (d *UnorderedStringMap) Remove(key string) {
 		sz := d.Len()
 		delete(d.stringToInt, key)
 		d.intToString[value.index] = d.intToString[sz-1]
+		d.intToString[sz-1] = ""
 		d.intToString = d.intToString[:sz-1]
 	}
 }
