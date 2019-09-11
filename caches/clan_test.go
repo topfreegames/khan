06741e9b08eab42fa3f3c4d0384aa7dc33aa794a
Matheus Pimenta <matheuscscp@gmail.com>
2019-09-11 14:35:13 -0300

    Refactoring tests to avoid too much code duplication

--- caches/clan_test.go
+++ caches/clan_test.go
@@ -20,55 +20,81 @@ var _ = Describe("Clan Cache", func() {
 	})
 
 	Describe("Clans Summaries", func() {
+		getPublicIDsAndIDToIndexMap := func(clans []*models.Clan) ([]string, map[string]int) {
+			var publicIDs []string
+			idToIdx := make(map[string]int)
+			for i, clan := range clans {
+				publicIDs = append(publicIDs, clan.PublicID)
+				idToIdx[clan.PublicID] = i + 1
+			}
+			return publicIDs, idToIdx
+		}
+
+		assertFirstCacheCall := func(clans []*models.Clan, idToIdx map[string]int, clansSummaries []map[string]interface{}) {
+			Expect(len(clansSummaries)).To(Equal(len(clans)))
+			for _, clanPayload := range clansSummaries {
+				// assert public ID
+				publicID, ok := clanPayload["publicID"].(string)
+				Expect(ok).To(BeTrue())
+				Expect(idToIdx[publicID]).To(BeNumerically(">", 0))
+
+				// assert name
+				name, ok := clanPayload["name"].(string)
+				Expect(ok).To(BeTrue())
+				Expect(name).To(Equal(clans[idToIdx[publicID]-1].Name))
+			}
+		}
+
+		updateClan := func(db models.DB, clan *models.Clan) {
+			clan.Name = "different name"
+			_, err := db.Update(clan)
+			Expect(err).NotTo(HaveOccurred())
+		}
+
+		assertSecondCacheCall := func(clans []*models.Clan, clansSummaries, secondClansSummaries []map[string]interface{}, shouldBeChanged bool) {
+			Expect(len(secondClansSummaries)).To(Equal(len(clans)))
+
+			// assert public ID
+			secondPayload := secondClansSummaries[0]
+			secondPublicID, ok := secondPayload["publicID"].(string)
+			Expect(ok).To(BeTrue())
+			Expect(secondPublicID).To(Equal(clans[0].PublicID))
+
+			// assert name
+			firstName, ok := clansSummaries[0]["name"].(string)
+			Expect(ok).To(BeTrue())
+			secondName, ok := secondPayload["name"].(string)
+			Expect(ok).To(BeTrue())
+			if !shouldBeChanged {
+				Expect(secondName).To(Equal(firstName))
+				Expect(secondName).NotTo(Equal(clans[0].Name))
+			} else {
+				Expect(secondName).NotTo(Equal(firstName))
+				Expect(secondName).To(Equal(clans[0].Name))
+			}
+		}
+
 		It("Should return a cached payload for a second call made immediately after the first", func() {
 			gameID := uuid.NewV4().String()
 			_, clans, err := models.GetTestClans(testDb, gameID, "test-sort-clan", 10)
 			Expect(err).NotTo(HaveOccurred())
 
-			var publicIDs []string
-			clansMap := make(map[string]int)
-			for i, clan := range clans {
-				publicIDs = append(publicIDs, clan.PublicID)
-				clansMap[clan.PublicID] = i + 1
-			}
+			publicIDs, idToIdx := getPublicIDsAndIDToIndexMap(clans)
 
 			cache := testing.GetTestClansSummariesCache()
 
 			// first call
 			clansSummaries, err := cache.GetClansSummaries(testDb, gameID, publicIDs)
 			Expect(err).NotTo(HaveOccurred())
-			Expect(len(clansSummaries)).To(Equal(len(clans)))
-			for _, clanPayload := range clansSummaries {
-				// assert public ID
-				publicID, ok := clanPayload["publicID"].(string)
-				Expect(ok).To(BeTrue())
-				Expect(clansMap[publicID]).To(BeNumerically(">", 0))
-
-				// assert name
-				name, ok := clanPayload["name"].(string)
-				Expect(ok).To(BeTrue())
-				Expect(name).To(Equal(clans[clansMap[publicID]-1].Name))
-			}
+			assertFirstCacheCall(clans, idToIdx, clansSummaries)
 
 			// update a clan
-			clans[0].Name = "different name"
-			_, err = testDb.Update(clans[0])
-			Expect(err).NotTo(HaveOccurred())
+			updateClan(testDb, clans[0])
 
 			// second call
 			secondClansSummaries, err := cache.GetClansSummaries(testDb, gameID, publicIDs)
 			Expect(err).NotTo(HaveOccurred())
-			Expect(len(secondClansSummaries)).To(Equal(len(clans)))
-			secondPayload := secondClansSummaries[0]
-			secondPublicID, ok := secondPayload["publicID"].(string)
-			Expect(ok).To(BeTrue())
-			Expect(secondPublicID).To(Equal(clans[0].PublicID))
-			firstName, ok := clansSummaries[0]["name"].(string)
-			Expect(ok).To(BeTrue())
-			secondName, ok := secondPayload["name"].(string)
-			Expect(ok).To(BeTrue())
-			Expect(secondName).To(Equal(firstName))
-			Expect(secondName).NotTo(Equal(clans[0].Name))
+			assertSecondCacheCall(clans, clansSummaries, secondClansSummaries, false)
 		})
 
 		It("Should return fresh information after expiration time is reached", func() {
@@ -76,12 +102,7 @@ var _ = Describe("Clan Cache", func() {
 			_, clans, err := models.GetTestClans(testDb, gameID, "test-sort-clan", 10)
 			Expect(err).NotTo(HaveOccurred())
 
-			var publicIDs []string
-			clansMap := make(map[string]int)
-			for i, clan := range clans {
-				publicIDs = append(publicIDs, clan.PublicID)
-				clansMap[clan.PublicID] = i + 1
-			}
+			publicIDs, idToIdx := getPublicIDsAndIDToIndexMap(clans)
 
 			cache := testing.GetTestClansSummariesCache()
 			cache.TTL = time.Second / 4
@@ -89,39 +110,16 @@ var _ = Describe("Clan Cache", func() {
 			// first call
 			clansSummaries, err := cache.GetClansSummaries(testDb, gameID, publicIDs)
 			Expect(err).NotTo(HaveOccurred())
-			Expect(len(clansSummaries)).To(Equal(len(clans)))
-			for _, clanPayload := range clansSummaries {
-				// assert public ID
-				publicID, ok := clanPayload["publicID"].(string)
-				Expect(ok).To(BeTrue())
-				Expect(clansMap[publicID]).To(BeNumerically(">", 0))
-
-				// assert name
-				name, ok := clanPayload["name"].(string)
-				Expect(ok).To(BeTrue())
-				Expect(name).To(Equal(clans[clansMap[publicID]-1].Name))
-			}
+			assertFirstCacheCall(clans, idToIdx, clansSummaries)
 
 			// update a clan
-			clans[0].Name = "different name"
-			_, err = testDb.Update(clans[0])
-			Expect(err).NotTo(HaveOccurred())
+			updateClan(testDb, clans[0])
 			time.Sleep(time.Second)
 
 			// second call
 			secondClansSummaries, err := cache.GetClansSummaries(testDb, gameID, publicIDs)
 			Expect(err).NotTo(HaveOccurred())
-			Expect(len(secondClansSummaries)).To(Equal(len(clans)))
-			secondPayload := secondClansSummaries[0]
-			secondPublicID, ok := secondPayload["publicID"].(string)
-			Expect(ok).To(BeTrue())
-			Expect(secondPublicID).To(Equal(clans[0].PublicID))
-			firstName, ok := clansSummaries[0]["name"].(string)
-			Expect(ok).To(BeTrue())
-			secondName, ok := secondPayload["name"].(string)
-			Expect(ok).To(BeTrue())
-			Expect(secondName).NotTo(Equal(firstName))
-			Expect(secondName).To(Equal(clans[0].Name))
+			assertSecondCacheCall(clans, clansSummaries, secondClansSummaries, true)
 		})
 	})
 })
