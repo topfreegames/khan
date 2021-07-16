b0f5553efeae8717de338c7ef380a04debfe7dab
Arthur Gonçalves <arthur29@hotmail.com.br>
2021-07-16 11:34:42 -0300

    Improve text search route (#92)
    Co-authored-by: Arthur Nogueira Gonçalves <arthur.nogueira@wildlifestudios.com>

--- mongo/helpers.go
+++ mongo/helpers.go
@@ -16,6 +16,10 @@ func GetClanNameTextIndexCommand(gameID string, background bool) bson.D {
 					"name":         "text",
 					"namePrefixes": "text",
 				},
+				"weights": bson.M{
+					"name":         256,
+					"namePrefixes": 1,
+				},
 				"name":             fmt.Sprintf("clans_%s_name_text_namePrefixes_text_index", gameID),
 				"background":       background,
 				"default_language": "none",
